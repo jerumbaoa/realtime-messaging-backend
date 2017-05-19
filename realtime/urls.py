80ab60797d88fda268a819dc9638d77617db55f6
Jerome Rumbaoa <jerumbaoa@gmail.com>
2017-05-19 08:56:42 +0800

    initial commit

---
+++ realtime/urls.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.views.generic import TemplateView
+
+
+urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name="base.html"), name="base"),
+    url(r'^admin/', admin.site.urls),
+
+    url(r'^api/chat/', include('chats.urls')),
+]
